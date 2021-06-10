b799071b6b8f306c86837fe5ad5c456fb650744f
waterfog9580 <waterfog9580@gmail.com>
2021-06-10 14:59:16 +0900

    update

--- isInCircle.py
+++
@@ -1,8 +0,0 @@
-import math
-# 유클리드 거리 계산 메서드
-def isInCircle(ax, ay, bx, by, radius):
-    _len = math.sqrt((ax-bx)**2 + (ay-by)**2) 
-    if _len <= radius:
-        return True
-    else:
-        return False
