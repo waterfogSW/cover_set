c02e500a611a7cafa5854ff33e777d9e103c39d6
kang <rilac1@naver.com>
2021-06-11 10:41:54 +0900

    0611

--- Scatter_circles.py
+++ Scatter_circles.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 # 시각화
 def scatter_circles(selected_point, radius, data):
+    title = './result/r_' + str(radius) + ".png"
 
     fig, ax_nstd = plt.subplots(figsize=(8, 8))
 
@@ -9,4 +10,4 @@ def scatter_circles(selected_point, radius, data):
         plt.scatter(Cx, Cy, color = 'k', marker = 'x', s = 10)
         ax_nstd.add_patch(plt.Circle((Cx, Cy), radius, color='g', alpha=0.3))
     plt.show()
-    plt.savefig('scatter.png')
+    plt.savefig(title)
