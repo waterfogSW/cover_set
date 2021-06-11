330b8b85f78ea3050aaa544febd33f0459d0b9fd
kang <rilac1@naver.com>
2021-06-11 10:52:12 +0900

    0611

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import os
 
 from Cover_set_test import cover_set_test
 
-cover_set_test(3,15,"scp_data.csv")
 os.system("chmod +x count.sh")
 os.system("./count.sh")
+
+cover_set_test(3,15,"scp_data.csv")
+
